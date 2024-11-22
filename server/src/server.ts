6e0d26910348343ca891df839b1e9397979a8738
Erik Pedersen <59267386+ezpedersen@users.noreply.github.com>
2024-11-21 20:59:19 -0500

    added mongodb connection with env file reading

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,13 @@
 // @deno-types="npm:@types/express"
 import express, { NextFunction, Request, Response } from "npm:express";
+import "jsr:@std/dotenv/load";
+import mongoose from "npm:mongoose";
 
 const app = express();
-
 const PORT = Number(Deno.env.get("PORT")) || 3000;
+const MONGODB_URI = String(Deno.env.get("MONGODB_URI"));
+
+await mongoose.connect(MONGODB_URI);
 
 app.get("/", (_req, res) => {
     res.status(200).send("Testing")
