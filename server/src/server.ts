1f9f3534271f437232d8b90f5edb9fa691cc2d2f
trishan104@gmail.com <trishan104@gmail.com>
2024-11-21 22:13:11 -0500

    route created: get user by id

--- server/src/server.ts
+++ server/src/server.ts
@@ -13,6 +13,28 @@ app.get("/", (_req, res) => {
     res.status(200).send("Testing")
 })
 
+
+import User from "../model/User.ts";
+//get specific user by id
+app.get('/user/:uid', async (req, res) =>{
+    const {uid} = req.params;
+    const user = await User.findOne({id: uid});
+
+    if (!user){
+        res.status(404).json({error: "user not found"});
+    }
+    try{
+        res.status(200).json(user);
+    }
+    catch(err){
+            if (err instanceof Error) {
+                res.status(500).json({ error: err.message });
+            } else {
+                res.status(500).json({ error: 'An unknown error occurred' });
+            }
+        }
+})
+
 app.listen(PORT, ()=>{
     console.log(`Listening on port ${PORT}`)
 })
