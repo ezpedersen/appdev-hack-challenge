8dc56cf58f1eb8a11bc58065b0fc3df625c0725d
Erik Pedersen <59267386+ezpedersen@users.noreply.github.com>
2024-11-21 20:16:06 -0500

    basic express setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,13 @@
-import express from 'express' 
-import dotenv from "dotenv";
+// @deno-types="npm:@types/express"
+import express, { NextFunction, Request, Response } from "npm:express";
 
 const app = express();
-dotenv.config();
 
-const PORT = 8080;
+const PORT = Number(Deno.env.get("PORT")) || 3000;
 
+app.get("/", (_req, res) => {
+    res.status(200).send("Testing")
+})
 app.listen(PORT, ()=>{
     console.log(`Listening on port ${PORT}`)
 })
