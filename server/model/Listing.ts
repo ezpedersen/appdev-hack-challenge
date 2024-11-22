ede3a5b0c51fb61769f6ab3dbd404eda4e82a168
trishan104@gmail.com <trishan104@gmail.com>
2024-11-21 21:29:16 -0500

    listing schema updated

--- server/model/Listing.ts
+++ server/model/Listing.ts
@@ -1,10 +1,18 @@
 import {model, Schema} from "npm:mongoose"
 
-const listingSchema = new Schema({
-   name: {type: String},
-   createdAt: {type: Date, default: Date.now},
-   type: {type: String}, // Enum offering or request
-   // add references to two users: one who will accept the item and one who will give the item
-})
-
-export default model("Listing", listingSchema);
+const listingSchema = new Schema(
+   {
+     name: { type: String, required: true },
+     createdAt: { type: Date, default: Date.now },
+     description: { type: String, required: true },
+     type: {
+       type: String,
+       enum: ["offering", "request"], 
+       required: true,
+     },
+     owner: { type: Schema.Types.ObjectId, ref: "User", required: true },
+     acceptedBy: { type: Schema.Types.ObjectId, ref: "User" }, 
+   }
+ );
+ 
+ export default model("Listing", listingSchema);
