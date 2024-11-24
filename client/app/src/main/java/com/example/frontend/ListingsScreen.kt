e7986f7af3e1f6ba22c712e33e45417c6b50424b
CBT83066 <pariskritacharya054@gmail.com>
2024-11-24 16:44:36 -0500

    previews

--- client/app/src/main/java/com/example/frontend/ListingsScreen.kt
+++ client/app/src/main/java/com/example/frontend/ListingsScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
+@Preview(showBackground = true)
 @Composable
 fun ListingsScreen() {
     LazyColumn (
--- client/app/src/main/java/com/example/frontend/ProfileScreen.kt
+++ client/app/src/main/java/com/example/frontend/ProfileScreen.kt
@@ -12,9 +12,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
-
+@Preview(showBackground = true)
 @Composable
 fun ProfileScreen() {
     Column(modifier = Modifier.fillMaxSize(),
