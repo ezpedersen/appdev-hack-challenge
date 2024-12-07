deb77d7421cca28367b4b2beca7c3fde885a7271
CBT83066 <pariskritacharya054@gmail.com>
2024-12-06 23:40:16 -0500

    Update MyListingsScreen.kt

--- client/app/src/main/java/com/example/frontend/MyListingsScreen.kt
+++ client/app/src/main/java/com/example/frontend/MyListingsScreen.kt
@@ -94,7 +94,7 @@ fun MyListingsScreen(logOutModel: LoginViewModel = hiltViewModel()
 
     val shouldShowDialog = remember { mutableStateOf(false) }
 
-    var listing: Listing=  Listing("empty", Date.valueOf(LocalDate.now().toString()), "empty", "empty", "", proxyUser, proxyUser)
+    var listing: Listing=  Listing("Screwdriver", Date.valueOf(LocalDate.now().toString()), "noImage", "philips head", "tool", proxyUser, proxyUser)
 
     if (shouldShowDialog.value) {
         AddListingDialog(
@@ -179,12 +179,12 @@ fun ListingTab(listing: Listing) {
         .fillMaxWidth()
     ) {
         Column{
-            Text(text = listing.name, color = Color.DarkGray)
-            Text(text = listing.createdAt.toString(), color = Color.DarkGray)
-            Text(text = listing.description, color = Color.DarkGray)
-            Text(text = listing.type, color = Color.DarkGray)
-            Text(text = listing.owner.name, color = Color.DarkGray)
-            Text(text = listing.acceptedBy.name, color = Color.DarkGray)
+            Text(text = "Item: ${listing.name}", color = Color.DarkGray)
+            Text(text = "Item posted: ${listing.createdAt}", color = Color.DarkGray)
+            Text(text = "Description: ${listing.description}", color = Color.DarkGray)
+            Text(text = "Listing type: ${listing.type}", color = Color.DarkGray)
+            Text(text = "Provider: ${listing.owner.name}", color = Color.DarkGray)
+            Text(text = "Accepted by: ${listing.acceptedBy.name}", color = Color.DarkGray)
         }
     }
 }
